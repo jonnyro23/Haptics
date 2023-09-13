e5acb956d00b11e5107375d9000e89eecde386cc
Roma Dudka <jonnyro23@gmail.com>
2023-09-13 09:10:29 +0300

    initial version

--- Sources/Haptics/Haptics.swift
+++ Sources/Haptics/Haptics.swift
@@ -1,6 +1,64 @@
-public struct Haptics {
-    public private(set) var text = "Hello, World!"
+import UIKit
 
-    public init() {
+// MARK: - Haptics
+
+public struct Haptics {
+    
+    static private var instance = Haptics()
+    
+    private var notificationGenerator = UINotificationFeedbackGenerator()
+    private var impactGenerator: ImpactFeedbackGenerator = .init()
+    
+    public enum Notification {
+        case success
+        case warning
+        case error
     }
+
+    public enum Impact {
+        case light
+        case medium
+        case heavy
+    }
+    
+    func prepare() {
+        notificationGenerator.prepare()
+        impactGenerator.heavy.prepare()
+        impactGenerator.medium.prepare()
+        impactGenerator.light.prepare()
+    }
+
+    public func generate(_ notification: Haptics.Notification) {
+        notificationGenerator.prepare()
+        switch notification {
+        case .success:
+            notificationGenerator.notificationOccurred(.success)
+        case .warning:
+            notificationGenerator.notificationOccurred(.warning)
+        case .error:
+            notificationGenerator.notificationOccurred(.error)
+        }
+    }
+
+    public func generate(_ impact: Haptics.Impact) {
+        switch impact {
+        case .light:
+            impactGenerator.light.impactOccurred()
+        case .medium:
+            impactGenerator.medium.impactOccurred()
+        case .heavy:
+            impactGenerator.heavy.impactOccurred()
+        }
+    }
+    
+}
+
+// MARK: - ImpactFeedbackGenerator
+
+private struct ImpactFeedbackGenerator {
+    var light = UIImpactFeedbackGenerator(style: .light)
+
+    var medium = UIImpactFeedbackGenerator(style: .medium)
+
+    var heavy = UIImpactFeedbackGenerator(style: .heavy)
 }
--- Tests/HapticsTests/HapticsTests.swift
+++ Tests/HapticsTests/HapticsTests.swift
@@ -6,6 +6,6 @@ final class HapticsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Haptics().text, "Hello, World!")
+//        XCTAssertEqual(Haptics().text, "Hello, World!")
     }
 }
